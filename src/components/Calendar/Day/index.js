f04de14e6030f569d0a1a60e566d65f7d2be61f1
TyrantJane <nonstop00066@gmail.com>
2022-08-15 03:20:16 +0300

    Add classes to Day component

--- src/components/Calendar/Day/index.js
+++ src/components/Calendar/Day/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { format } from "date-fns";
+import styles from "./Day.module.scss";
 
 const Day = (props) => {
   const { currentDate } = props;
   return (
-    <div>
-      <p>{format(currentDate, "EEEE")}</p>
-      <p>{format(currentDate, "d")}</p>
-    </div>
+    <article className={styles.article}>
+      <p className={styles["week-day"]}>{format(currentDate, "EEEE")}</p>
+      <p className={styles["month-day"]}>{format(currentDate, "d")}</p>
+    </article>
   );
 };
-// Friday, 29 July, 2022 year
 
 Day.defaultProps = {
   currentDate: new Date(),
